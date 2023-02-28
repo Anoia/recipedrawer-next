91f116ada680c8332f3c9e575f38bdbe23c1b75d
Anoia <github@stuckinadrawer.com>
2023-02-28 14:51:15 +0100

    Make recipelist more responsive

--- components/recipelist.tsx
+++ components/recipelist.tsx
@@ -12,10 +12,10 @@ function RecipeCard(props: any) {
   const recipe = props.recipe
 
   return (
-    <div className=" py-6 px-6 w-1/3 ">
+    <div className=" py-6 px-6">
       <Link href={`/recipe/${recipe.id}`}>
         <div className=" border shadow-md border-gray-300 max-w-sm h-80 flex flex-col">
-          <div className="min-w-[80px] bg-slate-600 flex-1 relative">
+          <div className="bg-slate-600 flex-1 relative">
             {recipe.image && (
               <Image
                 className=" "
@@ -60,7 +60,7 @@ function RecipeList() {
 
   return (
     <div className="container mx-auto my-12 max-w-5xl">
-      <div className="flex flex-wrap -mx-3 overflow-hidden">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  -mx-3 overflow-hidden">
         {data.map((recipe) => (
           <RecipeCard recipe={recipe} key={recipe.id} />
         ))}
